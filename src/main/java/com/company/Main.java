7acfa429a94132b844332af0ff9f895a7040ff8e
Szczukox <33254494+Szczukox@users.noreply.github.com>
2019-11-06 22:49:34 +0100

    + Fixed bug with heuristic algorithm

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -15,55 +15,67 @@ public class Main {
 
     public static void main(String[] args) {
 
+//        int[][] distanceMatrix = DatasetParser.loadDatasetEuc2D(new File(EUC_2D_INSTANCES_PATH + BERLIN52 + INSTANCE_EXTENSION).getAbsolutePath());
+//        for (int[] row : distanceMatrix) {
+//            for (int distance : row) {
+//                System.out.print(distance + " ");
+//            }
+//            System.out.println();
+//        }
+//
+//        int[] optimalPermutation = DatasetParser.loadOptimalPermutation(new File(EUC_2D_OPTIMAL_RESULT_PATH + BERLIN52 + OPTIMAL_RESULT_EXTENSION).getAbsolutePath());
+//
+//        int optimalDistance = ResultCalculator.calculateTotalDistance(optimalPermutation, distanceMatrix);
+//        System.out.println(optimalDistance);
+//
+//        double similarity = ResultCalculator.calculateSolutionSimilarity(optimalPermutation, optimalPermutation);
+//        System.out.println(similarity);
+//
+//        for (String filename : FILENAMES) {
+//            System.out.println(filename);
+//        }
+//
+//        int[] greedyPath = GreedyHeuristicsAlgorithm.findPath(distanceMatrix);
+//        for (int place : greedyPath) {
+//            System.out.print(place + " ");
+//        }
+
+
         int[][] distanceMatrix = DatasetParser.loadDatasetEuc2D(new File(EUC_2D_INSTANCES_PATH + BERLIN52 + INSTANCE_EXTENSION).getAbsolutePath());
-        for (int[] row : distanceMatrix) {
-            for (int distance : row) {
-                System.out.print(distance + " ");
-            }
-            System.out.println();
-        }
+
+        int[] greedyPath = GreedyLocalSearchAlgorithm.findPath(distanceMatrix);
+        int[] steepestPath = SteepestLocalSearchAlgorithm.findPath(distanceMatrix);
+        int[] randomPath = RandomAlgorithm.findPath(distanceMatrix);
+        int[] heuristicPath = GreedyHeuristicsAlgorithm.findPath(distanceMatrix);
+
+        int greedyDistance = ResultCalculator.calculateTotalDistance(greedyPath, distanceMatrix);
+        int steepestDistance = ResultCalculator.calculateTotalDistance(steepestPath, distanceMatrix);
+        int randomDistance = ResultCalculator.calculateTotalDistance(randomPath, distanceMatrix);
+        int heuristicDistance = ResultCalculator.calculateTotalDistance(heuristicPath, distanceMatrix);
 
         int[] optimalPermutation = DatasetParser.loadOptimalPermutation(new File(EUC_2D_OPTIMAL_RESULT_PATH + BERLIN52 + OPTIMAL_RESULT_EXTENSION).getAbsolutePath());
-
         int optimalDistance = ResultCalculator.calculateTotalDistance(optimalPermutation, distanceMatrix);
-        System.out.println(optimalDistance);
 
-        double similarity = ResultCalculator.calculateSolutionSimilarity(optimalPermutation, optimalPermutation);
-        System.out.println(similarity);
-
-        for (String filename : FILENAMES) {
-            System.out.println(filename);
-        }
-
-        int[] greedyPath = GreedyHeuristicsAlgorithm.findPath(distanceMatrix);
-        for (int place : greedyPath) {
-            System.out.print(place + " ");
-        }
-
-
-        int[][] distanceMatrix2 = DatasetParser.loadDatasetEuc2D(new File(EUC_2D_INSTANCES_PATH + BERLIN52 + INSTANCE_EXTENSION).getAbsolutePath());
-        int[] greedyPath2 = GreedyLocalSearchAlgorithm.findPath(distanceMatrix2);
-        int[] steepestPath = SteepestLocalSearchAlgorithm.findPath(distanceMatrix2);
-        int[] randomPath = RandomAlgorithm.findPath(distanceMatrix2);
-        int distanceGreedy = ResultCalculator.calculateTotalDistance(greedyPath2, distanceMatrix2);
-        int distanceSteepest = ResultCalculator.calculateTotalDistance(steepestPath, distanceMatrix2);
-        int distanceRandom = ResultCalculator.calculateTotalDistance(randomPath, distanceMatrix2);
         System.out.println();
-        System.out.println("Greedy local search: " + Integer.toString(distanceGreedy));
-        System.out.println("Steepest local search: " + Integer.toString(distanceSteepest));
-        System.out.println("Random: " + Integer.toString(distanceRandom));
-        System.out.print("Optimal: " + Integer.toString(optimalDistance));
+        System.out.println("Greedy local search: " + greedyDistance);
+        System.out.println("Steepest local search: " + steepestDistance);
+        System.out.println("Random: " + randomDistance);
+        System.out.println("Heuristic: " + heuristicDistance);
+        System.out.println("Optimal: " + optimalDistance);
 
 
         int counter = 0;
-        long time0 = System.currentTimeMillis();
-
+        long endTime;
+        long startTime = System.currentTimeMillis();
         do {
-            GreedyLocalSearchAlgorithm.findPath(distanceMatrix2);
+            GreedyLocalSearchAlgorithm.findPath(distanceMatrix);
+            endTime = System.currentTimeMillis();
             counter++;
-        } while (System.currentTimeMillis() - time0 < 1000 || counter < 10);
+        } while (endTime - startTime < 1000 || counter < 10);
 
-        float t = (float) (System.currentTimeMillis() - time0) / counter;
+        System.out.println(counter);
+
+        float t = (float) (endTime- startTime) / counter;
         System.out.println("\nAverage time Greedy local search: " + t);
     }
 }
--- src/main/java/com/company/algorithm/GreedyHeuristicsAlgorithm.java
+++ src/main/java/com/company/algorithm/GreedyHeuristicsAlgorithm.java
@@ -5,27 +5,32 @@ import java.util.Random;
 public class GreedyHeuristicsAlgorithm {
 
     public static int[] findPath(int[][] distanceMatrix) {
-        int[] greedyPath = new int[distanceMatrix.length];
-
-
-        int firstPlace = new Random().nextInt(distanceMatrix.length) + 1;
-        greedyPath[0] = firstPlace;
-        for (int i = 0; i < distanceMatrix.length; i++) {
-            distanceMatrix[i][firstPlace - 1] = 0;
+        int[][] cloneDistanceMatrix = new int[distanceMatrix.length][];
+        for (int i = 0; i< distanceMatrix.length; i++) {
+            cloneDistanceMatrix[i] = distanceMatrix[i].clone();
         }
-        for (int i = 1; i < distanceMatrix.length; i++) {
+
+        int[] greedyPath = new int[cloneDistanceMatrix.length];
+
+
+        int firstPlace = new Random().nextInt(cloneDistanceMatrix.length) + 1;
+        greedyPath[0] = firstPlace;
+        for (int i = 0; i < cloneDistanceMatrix.length; i++) {
+            cloneDistanceMatrix[i][firstPlace - 1] = 0;
+        }
+        for (int i = 1; i < cloneDistanceMatrix.length; i++) {
             int minDistance = Integer.MAX_VALUE;
             int minDistancePlace = 0;
-            for (int j = 0; j < distanceMatrix.length; j++) {
-                int currentDistance = distanceMatrix[greedyPath[i - 1] - 1][j];
+            for (int j = 0; j < cloneDistanceMatrix.length; j++) {
+                int currentDistance = cloneDistanceMatrix[greedyPath[i - 1] - 1][j];
                 if (currentDistance != 0 && currentDistance < minDistance) {
                     minDistance = currentDistance;
                     minDistancePlace = j + 1;
                 }
             }
             greedyPath[i] = minDistancePlace;
-            for (int j = 0; j < distanceMatrix.length; j++) {
-                distanceMatrix[j][minDistancePlace - 1] = 0;
+            for (int j = 0; j < cloneDistanceMatrix.length; j++) {
+                cloneDistanceMatrix[j][minDistancePlace - 1] = 0;
             }
         }
         return greedyPath;
