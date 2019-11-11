e9a07ea72ee024cce3a3a8b43437a77dd3e9e330
Michaellos <kalczynski.michal@gmail.com>
2019-11-11 14:02:06 +0100

    Added basic charts for Task 5

--- src/main/resources/charts_generator/charts.py
+++ src/main/resources/charts_generator/charts.py
@@ -4,22 +4,40 @@ import matplotlib.pyplot as plt
 
 path_to_charts = "../out/charts/"
 
-def save_plot(plot, name):
+def save_plot(plot, name, task):
 
     if not os.path.exists(path_to_charts):
         os.mkdir(path_to_charts)
-    plot.savefig(path_to_charts + name)
+    if not os.path.exists(path_to_charts + task):
+        os.mkdir(path_to_charts + task)
+    plot.savefig(path_to_charts + task + name)
 
 
 def task_4():
+    output_folder = "Task 4/"
     data = pd.read_csv('../out/results_task_4_berlin52.csv', sep=',')
-    type_of_result = ["AverageResults", "MinimumResults"]
-    alghoritm_name = ["GreedyLocalSearchAlgorithm", "SteepestLocalSearchAlgorithm"]
+    type_of_result = ["AverageDistance", "MinimumDistance"]
+    algorithm_name = ["GreedyLocalSearchAlgorithm", "SteepestLocalSearchAlgorithm"]
     for t in type_of_result:
-        for algorithm in alghoritm_name:
+        for algorithm in algorithm_name:
             result = data.loc[(data.Algorithm == algorithm) & (data.Type == t)]
-            plt.plot(result.iloc[0, 2:])
-            save_plot(plt, t+algorithm)
+            fig1, ax1 = plt.subplots(figsize=(10, 6))
+            ax1.plot(result.iloc[0, 2:])
+            save_plot(plt, t+algorithm, output_folder)
+
+
+def task_5():
+    output_folder = "Task 5/"
+    data = pd.read_csv('../out/results_task_5_berlin52.csv', sep=',')
+    type_of_result = ["Results", "Similarities"]
+    algorithm_name = ["GreedyLocalSearchAlgorithm", "SteepestLocalSearchAlgorithm", "RandomAlgorithm", "HeuristicsAlgorithm"]
+    for algorithm in algorithm_name:
+        quality = data.loc[(data.Algorithm == algorithm) & (data.Type == "Results")]
+        similarity = data.loc[(data.Algorithm == algorithm) & (data.Type == "Similarities")]
+        fig1, ax1 = plt.subplots(figsize=(10, 6))
+        ax1.plot(quality.iloc[0, 2:], similarity.iloc[0, 2:], 'bo')
+        save_plot(plt, algorithm, output_folder)
 
 
 task_4()
+task_5()
