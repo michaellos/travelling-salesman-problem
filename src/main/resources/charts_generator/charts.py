1ebaeb418f2797e8fe7e065df1ade4990a114beb
Michaellos <kalczynski.michal@gmail.com>
2019-11-11 20:10:38 +0100

    Added basic charts for Task 2

--- src/main/resources/charts_generator/charts.py
+++ src/main/resources/charts_generator/charts.py
@@ -14,6 +14,45 @@ def save_plot(plot, name, task):
     plot.savefig(path_to_charts + task + name)
 
 
+def task_2():
+    output_folder = "Task 2/"
+    data = pd.read_csv('../out/results_task_2_berlin52.csv', sep=',')
+    algorithm_names = data["Algorithm"].to_list()
+
+    average_results = data["AverageResult"].to_list()
+    standard_deviation = data["StandardDeviation"].to_list()
+    fig1, ax1 = plt.subplots(figsize=(10, 6))
+    plt.yscale("log")
+    ax1.errorbar(algorithm_names, average_results, standard_deviation, linestyle='None', fmt='o')
+    save_plot(plt, "AverageResult", output_folder)
+
+    best_results = data["BestResult"].to_list()
+    fig1, ax1 = plt.subplots(figsize=(10, 6))
+    plt.yscale("log")
+    ax1.plot(algorithm_names, best_results, 'bo')
+    save_plot(plt, "BestResult", output_folder)
+
+    average_times = data["AverageTime"].to_list()
+    fig1, ax1 = plt.subplots(figsize=(10, 6))
+    ax1.plot(algorithm_names, average_times, 'bo')
+    save_plot(plt, "AverageTime", output_folder)
+
+    efficiency = data["Efficiency"].to_list()
+    fig1, ax1 = plt.subplots(figsize=(10, 6))
+    ax1.plot(algorithm_names, efficiency, 'bo')
+    save_plot(plt, "Efficiency", output_folder)
+
+    average_step_number = data["AverageStepNumber"].iloc[0:2].to_list()
+    fig1, ax1 = plt.subplots(figsize=(10, 6))
+    ax1.plot(algorithm_names[0:2], average_step_number, 'bo')
+    save_plot(plt, "AverageStepNumber", output_folder)
+
+    average_visit_solution_number = data["AverageVisitSolutionNumber"].iloc[0:2].to_list()
+    fig1, ax1 = plt.subplots(figsize=(10, 6))
+    ax1.plot(algorithm_names[0:2], average_visit_solution_number, 'bo')
+    save_plot(plt, "AverageVisitSolutionNumber", output_folder)
+
+
 def task_3():
     output_folder = "Task 3/"
     data = pd.read_csv('../out/results_task_3_berlin52.csv', sep=',')
@@ -52,6 +91,7 @@ def task_5():
         save_plot(plt, algorithm, output_folder)
 
 
+task_2()
 task_3()
-# task_4()
-# task_5()
+task_4()
+task_5()
