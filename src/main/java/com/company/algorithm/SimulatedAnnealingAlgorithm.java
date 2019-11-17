e2584fbd614d62b01ac5c40423a54001df934d26
Michaellos <kalczynski.michal@gmail.com>
2019-11-17 13:16:02 +0100

    Change neighbor finding in Simulated annealing

--- src/main/java/com/company/algorithm/SimulatedAnnealingAlgorithm.java
+++ src/main/java/com/company/algorithm/SimulatedAnnealingAlgorithm.java
@@ -18,12 +18,50 @@ public class SimulatedAnnealingAlgorithm {
         }
     }
 
+    private static void reverseSwap(int[] cities, int a, int b) {
+        while (a < b) {
+            cities[a] ^= cities[b];
+            cities[b] ^= cities[a];
+            cities[a] ^= cities[b];
+            a++;
+            b--;
+        }
+    }
+
+
     public static ResultEntity findPath(int[][] distanceMatrix, int[] path) {
         return findPath(distanceMatrix, path, 100);
     }
 
-//    ToDo: improve performance
+    private static int calculateNewDistance(int oldDistance, int i, int j, int[][] distanceMatrix, int[] path) {
+        int cityA = path[i];
+        int cityB = path[j];
+        int cityBeforeA = 0;
+        int cityAfterB = 0;
+        if (i == 0)
+            cityBeforeA = path[distanceMatrix.length - 1];
+        else
+            cityBeforeA = path[i - 1];
+
+        if (j == distanceMatrix.length - 1)
+            cityAfterB = path[0];
+        else
+            cityAfterB = path[j + 1];
+
+        int newDistance = oldDistance;
+        if (cityBeforeA != cityB || cityA != cityAfterB) {
+            newDistance = newDistance - (distanceMatrix[cityBeforeA - 1][cityA - 1] + distanceMatrix[cityB - 1][cityAfterB - 1]);
+            newDistance += distanceMatrix[cityBeforeA - 1][cityB - 1] + distanceMatrix[cityA - 1][cityAfterB - 1];
+        }
+
+        return newDistance;
+    }
+
+    //    ToDo: improve performance
     public static ResultEntity findPath(int[][] distanceMatrix, int[] path, float temperature) {
+
+//        Parameter defining how many neighbour to look at at given temperature
+        final int s = 1;
 
         int n = distanceMatrix.length;
 
@@ -37,51 +75,57 @@ public class SimulatedAnnealingAlgorithm {
         int[] firstPath = path.clone();
         int stepCounter = 0;
         int solutionCounter = 0;
-        int[] bestPath = new int[n];
+        int[] bestPath = path.clone();
         int distance = ResultCalculator.calculateTotalDistance(path, distanceMatrix);
         int bestDistance = distance;
+        boolean change;
+        int newDistance;
 
         do {
-//            ToDo: add better condition probably depending on temperature (like stairs)
-            for (int i = 0; i < n; i++) {
-                generateNewPath(path, n);
 
-                // perhaps no need to calculate distance from scratch
-                int newDistance = ResultCalculator.calculateTotalDistance(path, distanceMatrix);
+            int counter = 0;
 
-                // ToDo: remove duplicated code
-                if (newDistance <= distance) {
-                    distance = newDistance;
-                    if (bestDistance > distance) {
-                        bestDistance = distance;
-                        bestPath = path.clone();
-                    }
-                }
-                else {
-                    if (Math.exp((distance - newDistance) / temperature) > Math.random())  {
-                        distance = newDistance;
-                        if (bestDistance > distance) {
-                            bestDistance = distance;
-                            bestPath = path.clone();
+            do {
+                change = false;
+                for (int i = 0; i < n - 1; i++) {
+                    for (int j = i + 1; j < n; j++) {
+                        counter++;
+                        newDistance = calculateNewDistance(distance, i, j, distanceMatrix, path);
+
+//                        ToDo: remove duplicated code
+                        if (newDistance <= distance) {
+                            distance = newDistance;
+                            reverseSwap(path, i, j);
+                            if (bestDistance > distance) {
+                                bestDistance = distance;
+                                bestPath = path.clone();
+                            }
+                            change = true;
+                            break;
+                        } else {
+                            if (Math.exp((distance - newDistance) / temperature) > Math.random()) {
+                                distance = newDistance;
+                                reverseSwap(path, i, j);
+                                change = true;
+                                break;
+                            }
                         }
                     }
+                    if (change)
+                        break;
                 }
-            }
+            } while (counter < s * n * 2);
+
             temperature = calculateTemperature(temperature);
 //            ToDo: add better stop condition
-        }while(temperature > 50);
+        } while (temperature > 10);
 
 //        Should it return ResultEntity?
         return new ResultEntity(firstPath, bestPath, stepCounter, solutionCounter);
     }
 
-//    ToDo: add better cooling function
+    //    ToDo: add better cooling function
     private static float calculateTemperature(float temperature) {
         return temperature - 1;
-    }
-
-    //    ToDo: implement finding new path
-    private static void generateNewPath(int[] path, int n) {
-        shuffleXOR(path, n);
     }
 }
